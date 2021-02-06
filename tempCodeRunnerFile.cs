016c7c6f08669f5d3034e0939c0d28a90a1c600d
Zachary Munson <zmunson85@gmail.com>
2021-02-06 03:43:13 -0700

    Fundamentals Initial Commit

---
+++ tempCodeRunnerFile.cs
@@ -0,0 +1,21 @@
+  public static void print3or5()
+        {
+            for (int i = 1; i < 101; i++)
+            {
+
+                if (i % 3 == 0 && !(i % 3 == 0 && i % 5 == 0))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0 && !(i % 3 == 0 && i % 5 == 0))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+
+            }
+        }
