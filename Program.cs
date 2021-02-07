972b8c0908283c2270d550b93e06f751cb7e9528
Zachary Munson <zmunson85@gmail.com>
2021-02-06 20:51:48 -0700

    updated format

--- Program.cs
+++ Program.cs
@@ -1,42 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-
 namespace Fundamentals
+
+
+
+
+
+
+
 
 {
     class Program
     {
-        // public static void print255()
-        // {
-        //     for (int i = 1; i < 256; i++)
-        //     {
-        //         Console.WriteLine(i);
-        //     }
-        // }
-        public static void print3or5()
+        public static void print255()
         {
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < 256; i++)
             {
-
-                if (i % 3 == 0 && !(i % 3 == 0 && i % 5 == 0))
-                {
-                    Console.WriteLine("Fizz");
-                }else if (i % 5 == 0 && !(i % 3 == 0 && i % 5 == 0))
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-
-
+                Console.WriteLine(i);
             }
         }
+        // public static void print3or5()
+        // {
+        //     for (int i = 1; i < 101; i++)
+        //     {
+
+        //         if (i % 3 == 0 && !(i % 3 == 0 && i % 5 == 0))
+        //         {
+        //             Console.WriteLine("Fizz");
+        //         }else if (i % 5 == 0 && !(i % 3 == 0 && i % 5 == 0))
+        //         {
+        //             Console.WriteLine("Buzz");
+        //         }
+        //         else if (i % 3 == 0 && i % 5 == 0)
+        //         {
+        //             Console.WriteLine("FizzBuzz");
+        //         }
+
+
+        //     }
+        // }
         static void Main(string[] args)
         {
-            // print255();
-            print3or5();
+            print255();
+            // print3or5();
         }
     }
 }
+
+
+
+
+
+
+
+// {
+//     class Program
+// {
+//     public static void print3or5()
+//     {
+//         for (int i = 1; i < 101; i++)
+//         {
+
+//             if (i % 3 == 0 && !(i % 3 == 0 && i % 5 == 0))
+//             {
+//                 Console.WriteLine("Fizz");
+//             }
+//             else if (i % 5 == 0 && !(i % 3 == 0 && i % 5 == 0))
+//             {
+//                 Console.WriteLine("Buzz");
+//             }
+//             else if (i % 3 == 0 && i % 5 == 0)
+//             {
+//                 Console.WriteLine("FizzBuzz");
+//             }
+
+
+//         }
+//     }
+//     static void Main(string[] args)
+//     {
+//         print3or5();
+//     }
+// }
+// }
+
+
